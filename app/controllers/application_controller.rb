ce8894ccd1100aa6d9df2026443055054a00e134
Sonu SIngh <sonus.ror@gmail.com>
2018-09-01 17:08:22 +0530

    Removed sign up feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to root_path, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
-    end
-  end
+  before_action :no_sign_up
 
   def authenticate_admin!
     unless current_user.is_admin?    
       redirect_to new_user_session_path and return
     end 
-  end  
+  end
 
   
   protected
@@ -25,6 +18,12 @@ class ApplicationController < ActionController::Base
       admin_dashboard_index_path
     else
       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    end
+  end
+
+  def no_sign_up
+    if request.path == "/users/sign_up"
+      redirect_to new_user_session_path and return
     end
   end
 
